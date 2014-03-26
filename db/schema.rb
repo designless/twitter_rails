6c023a54b7fa76407a8a5d83db042a204f1b1653
Naoki Kanazawa <info@designless.net>
2014-03-26 22:56:55 +0900

    Design for HOME

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140325174902) do
+ActiveRecord::Schema.define(version: 20140325175556) do
 
   create_table "relationships", force: true do |t|
     t.integer  "requestor_id",                  null: false
